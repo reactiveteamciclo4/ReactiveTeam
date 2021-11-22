ee29865c3eb30c7db5b71bb966a64d18699e58d1
frankdia03 <frankdia03@gmail.com>
2021-11-22 10:32:24 -0800

    controller-model-router de proyecto

---
+++ controllers/proyecto.js
@@ -0,0 +1,74 @@
+
+const { response } = require('express')
+const Usuario = require('../models/proyecto')
+// const bcryptjs = require("bcryptjs");
+// const { validationResult } = require('express-validator');
+
+const getProyecto = (req, res) => {
+    const { nombre, apellido, edad } = req.query;
+    res.json({
+        msg: 'Api-get',
+
+        nombre,
+        apellido,
+    })
+}
+
+const postProyecto = async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty())// si existe errores entonces {
+    {
+        return res.status(400).json(errors)
+    }
+    const { nombre, objetivos, presupuesto, fechaInicio, fechaFin, lider, estado, fase, inscripciones, avances } = req.body;
+    const proyecto = new Proyecto({
+        nombre,
+        objetivos,
+        presupuesto,
+        fechaInicio,
+        fechaFin,
+        lider,
+        estado,
+        fase,
+        inscripciones,
+        avances
+    })
+
+    const existeProyecto = Proyecto.findOne({ nombre })
+
+    if (existeProyecto) {
+        return res.status(400).json({
+            msg: "Proyecto con ese nombre ya existe"
+        })
+    }
+
+    // const salt = bcryptjs.genSaltSync();
+    // usuario.password = bcryptjs.hashSync(password, salt);
+
+    await Proyecto.save();
+    res.json({
+        msg: 'Proyecto creado',
+        Proyecto
+
+    })
+}
+const putProyecto = (req, res) => {
+
+    const { id } = req.params;
+    res.json({
+        msg: 'Proyecto actualizado',
+        id
+    })
+}
+const deleteProyecto = (req, res) => {
+    res.json({
+        msg: 'Proyecto eliminado',
+    })
+}
+
+module.exports = {
+    getProyecto,
+    postProyecto,
+    putProyecto,
+    deleteProyecto
+}
