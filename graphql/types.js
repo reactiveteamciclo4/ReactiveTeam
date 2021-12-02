21b9dafa5be69f1e022092ab584413658dcc0a58
ZUKY <90728332+ZUKYMAZURY@users.noreply.github.com>
2021-12-02 18:58:53 -0500

    Proyecto y usuario

--- graphql/types.js
+++ graphql/types.js
@@ -1,8 +1,12 @@
 import { gql } from 'apollo-server-express';
 import { tiposAvance } from '../models/avance/tipos.js';
-
+import { tiposProyecto } from '../models/proyecto/tipos.js';
 const tiposGlobales = gql`
   scalar Date
 `;
 
-export const tipos = [tiposGlobales, tiposAvance];
+export const tipos = [
+  tiposGlobales,
+  tiposAvance,
+  tiposProyecto,
+];
