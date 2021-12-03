b5dde8bf509043a8ed50b3c4cf850462094b6ec3
Alejandra Bastidas <bastidasaleja@hotmail.com>
2021-12-03 10:24:45 -0500

    corriendo hasta coleccion de usuarios

---
+++ models/usuario/resolvers.js
@@ -0,0 +1,72 @@
+import { ModeloUsuario } from './usuario.js';
+import bcrypt from 'bcrypt';
+
+const resolversUsuario = {
+  Query: {
+    Usuarios: async (parent, args, context) => {
+      const usuarios = await ModeloUsuario.find().populate([
+        {
+          path: 'inscripciones',
+          populate: {
+            path: 'proyecto',
+            populate: [{ path: 'lider' }, { path: 'avances' }],
+          },
+        },
+        {
+          path: 'proyectosLiderados',
+        },
+      ]);
+      return usuarios;
+    },
+    Usuario: async (parent, args) => {
+      const usuario = await ModeloUsuario.findOne({ _id: args._id });
+      return usuario;
+    },
+  },
+  Mutation: {
+    crearUsuario: async (parent, args) => {
+      const salt = await bcrypt.genSalt(10);
+      const hashedPassword = await bcrypt.hash(args.password, salt);
+      const usuarioCreado = await ModeloUsuario.create({
+        nombre: args.nombre,
+        apellido: args.apellido,
+        identificacion: args.identificacion,
+        correo: args.correo,
+        rol: args.rol,
+        password: hashedPassword,
+      });
+
+      if (Object.keys(args).includes('estado')) {
+        usuarioCreado.estado = args.estado;
+      }
+
+      return usuarioCreado;
+    },
+    editarUsuario: async (parent, args) => {
+      const usuarioEditado = await ModeloUsuario.findByIdAndUpdate(
+        args._id,
+        {
+          nombre: args.nombre,
+          apellido: args.apellido,
+          identificacion: args.identificacion,
+          correo: args.correo,
+          estado: args.estado,
+        },
+        { new: true }
+      );
+
+      return usuarioEditado;
+    },
+    eliminarUsuario: async (parent, args) => {
+      if (Object.keys(args).includes('_id')) {
+        const usuarioEliminado = await ModeloUsuario.findOneAndDelete({ _id: args._id });
+        return usuarioEliminado;
+      } else if (Object.keys(args).includes('correo')) {
+        const usuarioEliminado = await ModeloUsuario.findOneAndDelete({ correo: args.correo });
+        return usuarioEliminado;
+      }
+    },
+  },
+};
+
+export { resolversUsuario };
