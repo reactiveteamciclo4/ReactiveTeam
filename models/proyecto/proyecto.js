5080347e02c7d88211b111e57a9e2cf47e160fba
Alejandra Bastidas <bastidasaleja@hotmail.com>
2021-12-05 08:08:50 -0500

    Correccion coleccion Proyectos

--- models/proyecto/proyecto.js
+++ models/proyecto/proyecto.js
@@ -4,64 +4,63 @@ const { Schema, model } = mongoose;
 
 const proyectoSchema = new mongoose.Schema({
 
-    nombre:{
-        type: String,
-        required: [true, 'El Nombre  es obligatorio'],
-      },
+  nombre: {
+    type: String,
+    required: [true, 'El Nombre  es obligatorio'],
+  },
 
-    presupuesto:{
-        type: Number,
-        required: [true, 'El presupuesto es obligatorio'],
-      },
+  presupuesto: {
+    type: Number,
+    required: [true, 'El presupuesto es obligatorio'],
+  },
 
-    fechaInicio: {
-        type: Date,
-        required: true,
-      },
- 
-    fechaFin: {
-        type: Date,
-        required: true,
-      },
+  fechaInicio: {
+    type: Date,
+    required: true,
+  },
 
-      lider: {
-        type: Schema.Types.ObjectId,
-        required: true,
-        ref: ModeloUsuario,
-    },
+  fechaFin: {
+    type: Date,
+    required: true,
+  },
 
-    estado: {
-      type: String,
-      default: 'INACTIVO ',
-      enum: ['INACTIVO', 'ACTIVO']
+  lider: {
+    type: Schema.Types.ObjectId,
+    required: true,
+    ref: ModeloUsuario,
+  },
+
+  estado: {
+    type: String,
+    default: 'INACTIVO ',
+    enum: ['INACTIVO', 'ACTIVO']
   },
   fase: {
     type: String,
     default: 'NULO ',
     enum: ['INICIADO', 'DESARROLLO ', 'TERMINADO', 'NULO']
-},
-
-    objetivos: [
-      {
-        descripcion: {
-          type: String,
-          required: [true, 'Descripcion es obligatorio'],
-        },
-        
-        tipo: {
-          type: String,
-          default: 'GENERAL ',
-          enum: ['GENERAL', 'ESPECIFICO'],
-          required: [true, 'Tipo es obligatorio'],
-        },
-      },
-    ],
   },
+
+  objetivos: [
+    {
+      descripcion: {
+        type: String,
+        required: [true, 'Descripcion es obligatorio'],
+      },
+
+      tipo: {
+        type: String,
+        enum: ['GENERAL', 'ESPECIFICO'],
+        required: [true, 'Tipo es obligatorio'],
+      },
+    },
+  ],
+},
   {
-    toJSON: { virtuals: true }, 
-    toObject: { virtuals: true }, 
+    toJSON: { virtuals: true },
+    toObject: { virtuals: true },
   }
-  );
+);
 
 
 proyectoSchema.virtual('avances', {
--- models/proyecto/tipos.js
+++ models/proyecto/tipos.js
@@ -1,73 +1,68 @@
 import { gql } from 'apollo-server-express';
 
 const tiposProyecto = gql`
-type Objetivo {
-  _id: ID!
-  descripcion: String!
-  tipo: Enum_TipoObjetivo!
-}
+  type Objetivo {
+    _id: ID!
+    descripcion: String!
+    tipo: Enum_TipoObjetivo!
+  }
 
-input crearObjetivo {
-  descripcion: String!
-  tipo: Enum_TipoObjetivo!
-}
+  input crearObjetivo {
+    descripcion: String!
+    tipo: Enum_TipoObjetivo!
+  }
 
-input camposObjetivo {
-  descripcion: String!
-  tipo: Enum_TipoObjetivo!
-}
+  input camposObjetivo {
+    descripcion: String!
+    tipo: Enum_TipoObjetivo!
+  }
 
-input camposProyecto {
-  nombre: String
-  presupuesto: Float
-  fechaInicio: Date
-  fechaFin: Date
-  estado: Enum_EstadoProyecto
-  fase: Enum_FaseProyecto
-  lider: String
-}
+  input camposProyecto {
+    nombre: String
+    presupuesto: Float
+    fechaInicio: Date
+    fechaFin: Date
+    estado: Enum_EstadoProyecto
+    fase: Enum_FaseProyecto
+    lider: String
+  }
 
-type Proyecto {
-  _id: ID!
-  nombre: String!
-  presupuesto: Float!
-  fechaInicio: Date!
-  fechaFin: Date!
-  estado: Enum_EstadoProyecto!
-  fase: Enum_FaseProyecto!
-  lider: Usuario!
-  objetivos: [Objetivo]
-  avances: [Avance]
-  inscripciones: [Inscripcion]
-}
-
-type Query {
-  Proyectos: [Proyecto]
-  filtrarProyecto(idProyecto: String!): [Proyecto]
-}
-
-type Mutation {
-  crearProyecto(
+  type Proyecto {
+    _id: ID!
     nombre: String!
     presupuesto: Float!
     fechaInicio: Date!
     fechaFin: Date!
     estado: Enum_EstadoProyecto!
     fase: Enum_FaseProyecto!
-    lider: String!
-    objetivos: [crearObjetivo]
-  ): Proyecto
+    lider: Usuario!
+    objetivos: [Objetivo]
+    avances: [Avance]
+    inscripciones: [Inscripcion]
+  }
 
-  editarProyecto(_id: String!, campos: camposProyecto!): Proyecto
-  eliminarProyecto(_id: String): Proyecto
+  type Query {
+    Proyectos: [Proyecto]
+  }
 
-  crearObjetivo(idProyecto: String!, campos: camposObjetivo!): Proyecto
+  type Mutation {
+    crearProyecto(
+      nombre: String!
+      presupuesto: Float!
+      fechaInicio: Date!
+      fechaFin: Date!
+      lider: String!
+      objetivos: [crearObjetivo]
+    ): Proyecto
 
-  editarObjetivo(idProyecto: String!, indexObjetivo: Int!, campos: camposObjetivo!): Proyecto
+    editarProyecto(_id: String!, campos: camposProyecto!): Proyecto
 
-  eliminarObjetivo(idProyecto: String!, idObjetivo: String!): Proyecto
-}
- 
+    crearObjetivo(idProyecto: String!, campos: camposObjetivo!): Proyecto
+
+    editarObjetivo(idProyecto: String!, indexObjetivo: Int!, campos: camposObjetivo!): Proyecto
+
+    eliminarObjetivo(idProyecto: String!, idObjetivo: String!): Proyecto
+  }
 `;
 
 export { tiposProyecto };
